f6dcdea13317ee98a99ebb5e0bf9f68382b9d0a0
SAMBIT SARGAM EKALABAYA <70655824+sambitsargam@users.noreply.github.com>
2023-05-18 19:41:24 +0530

    update

--- config/connectors.ts
+++
@@ -1,2 +0,0 @@
-import { InjectedConnector } from "@web3-react/injected-connector";
-export const Injected = new InjectedConnector({ supportedChainIds: [80001] });
--- lib/utils.ts
+++
@@ -1,5 +0,0 @@
-import { BigNumber } from 'ethers';
-
-export const bigTostring = (tx: BigNumber) => {
-  return tx.toString();
-};
